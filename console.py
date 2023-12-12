ded4db5be0cc9fca78142da35daa0b1c0689fb3a
simon <mutua7504@gmail.com>
2023-12-12 10:56:03 -0800

    mess

--- console.py
+++ console.py
@@ -1,4 +1,22 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 
 import cmd
-import models
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program cleanly with EOF"""
+        print("")
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
